15353c352faf47b3c99029279e991f3ba4a83fd8
Nurzod1 <nurzodkaramanov1@gmail.com>
2023-10-05 23:29:28 +0500

    added component ListFilter

---
+++ src/components/ListFilter.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+<template>
+  <div class="container">
+    <div class="text-white mt-[-28px]">
+      <div class="flex justify-center gap-2">
+        <input
+          class="outline-none bg-neutral-800 h-14 w-[630px] rounded-lg border border-black border-solid placeholder-gray-600 px-5 transition-all ease-in focus:border-neutral-700"
+          type="text"
+          placeholder="Введите слово"
+        />
+        <button
+          class="bg-sky-700 w-24 h-14 rounded-lg border border-black border-solid transition-all ease-in hover:bg-sky-600"
+        >
+          Поиск
+        </button>
+      </div>
+      <div class="flex justify-center items-center mt-5 gap-5">
+        <p class="text-sky-700 font-bold">Сортировать</p>
+        <select
+          class="bg-neutral-800 filter-form__select py-1.5 px-2.5 rounded"
+        >
+          <option>По дате создания</option>
+          <option>По дате изменения</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <main-navbar />
+    <list-filter />
   </div>
 </template>
 
 <script lang="ts">
+import ListFilter from "@/components/ListFilter.vue";
 import MainNavbar from "@/components/MainNavbar.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  components: { MainNavbar },
+  components: { MainNavbar, ListFilter },
   name: "HomeView",
 });
 </script>
